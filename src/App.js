bc0bcb61cb939cbf950e1cf4332f38bcc22e7f8c
VanillaGorilla <adrianlrivers@gmail.com>
2018-09-09 23:43:46 +0200

    Get user and comment data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.png";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    user: [],
+    comments: []
+  };
+
+  async componentDidMount() {
+    try {
+      const userData = await fetch("https://jsonplaceholder.typicode.com/users/1");
+      const commentData = await fetch("https://jsonplaceholder.typicode.com/posts?userId=1");
+      const res = await Promise.all([userData, commentData]);
+      const dataPromises = res.map(r => r.json());
+      const [user, comments] = await Promise.all(dataPromises);
+
+      this.setState({
+        user: user,
+        comments: comments
+      });
+
+      console.log(user, comments)
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
