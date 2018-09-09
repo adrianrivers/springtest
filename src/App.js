fc4124dd97004a2c111a666a1280fc939e0f97a4
VanillaGorilla <adrianlrivers@gmail.com>
2018-09-09 23:38:32 +0200

    Add logo and app template

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="Logo-container">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </div>
+        <div className="App-content">
+          <h3>Spring Coding Challenge</h3>
+        </div>
       </div>
     );
   }
